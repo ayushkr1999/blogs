3d13a6a36288dc5910cec674dc5f02f387efaf59
Ayushkr99 <39951210+Ayushkr99@users.noreply.github.com>
2020-09-13 18:39:37 +0530

    make it better 3

--- routes/articles.js
+++ routes/articles.js
@@ -1,226 +1,220 @@
 const express = require('express');
 const router = express.Router();
-const { ensureAuth, ensureGuest } = require('../middleware/auth')
+const { ensureAuth, ensureGuest } = require('../middleware/auth');
 const Article = require('../models/article.js');
 const Article2 = require('../models/article2.js');
-
+const User = require('../models/User.js');
 router.get('/', (req, res, next) => {
-  Article.getArticles((err,articles)=>{
-    res.render('articles',{
+  Article.getArticles((err, articles) => {
+    res.render('articles', {
       title: 'Articles',
-      articles:articles
+      articles: articles,
     });
   });
 });
 
 //show all admin articles
 router.get('/admin', (req, res, next) => {
-  Article2.getArticles((err,articles)=>{
-    res.render('articles2',{
+  Article2.getArticles((err, articles) => {
+    res.render('articles2', {
       title: 'Articlesadmin',
-      articles:articles
+      articles: articles,
     });
   });
 });
 
-
-
-router.get('/show/:id', (req, res, next) => {
-    Article.getArticleById(req.params.id, (err, article)=>{
-      // console.log(article.title);
-      res.render('article',{
-        title: 'Article',
-        article: article
-      });
-    });
-
+router.get('/show/:id', async (req, res, next) => {
+  // Article.getArticleById(req.params.id, (err, article)=>{
+  // console.log(article.title);
+  const article = await Article.findById(req.params.id);
+  const auth = await User.findById(article.user);
+  console.log(auth);
+  // console.log('hii');
+  res.render('article', {
+    title: 'Article',
+    article: article,
+    author: auth.firstName,
+  });
 });
+
+// });
 
 // get pushing articles
 router.get('/admin/show/:id', (req, res, next) => {
-    Article2.getArticleById(req.params.id, (err, article)=>{
-      // console.log(article.title);
-      res.render('article2',{
-        title: 'Articleadmin',
-        article: article
-      });
+  Article2.getArticleById(req.params.id, (err, article) => {
+    // console.log(article.title);
+    res.render('article2', {
+      title: 'Articleadmin',
+      article: article,
     });
-
+  });
 });
 
 // push article
-router.get('/admin/show/add/:id',(req,res,next)=>{
-
+router.get('/admin/show/add/:id', (req, res, next) => {
   // let article2 =new Article();
 
   // category.description=req.body.description;
-  Article2.getArticleById(req.params.id, (err, article)=>{
+  Article2.getArticleById(req.params.id, (err, article) => {
     // console.log(article.title);
     // console.log(article.title);
     // console.log(article.subtitle);
-    let article2 =new Article();
-    article2.title=article.title;
-    article2.subtitle=article.subtitle;
-    article2.category=article.category;
-    article2.body=article.body;
-    article2.user=article.user;
+    let article2 = new Article();
+    article2.title = article.title;
+    article2.subtitle = article.subtitle;
+    article2.category = article.category;
+    article2.body = article.body;
+    article2.user = article.user;
 
-    Article.addArticle(article2,(err, article2)=>{
-      if(err){
+    Article.addArticle(article2, (err, article2) => {
+      if (err) {
         res.send(err);
       }
       // req.flash('success','You Article will be added when its Verified');
       res.redirect('/articles/admin');
     });
   });
-
 });
 
 router.get('/category/:category_id', (req, res, next) => {
-  Article.getCategoryArticles(req.params.category_id,(err,articles)=>{
-    Category.getCategoryById(req.params.category_id,(err,category)=>{
-      res.render('articles',{
-        title: category.title+' Articles',
-        articles:articles
+  Article.getCategoryArticles(req.params.category_id, (err, articles) => {
+    Category.getCategoryById(req.params.category_id, (err, category) => {
+      res.render('articles', {
+        title: category.title + ' Articles',
+        articles: articles,
       });
-
     });
-
   });
-
 });
 
 //add artciles
-router.post('/add' ,ensureAuth ,(req,res,next)=>{
-  req.checkBody('title','Title is req').notEmpty();
+router.post('/add', ensureAuth, (req, res, next) => {
+  req.checkBody('title', 'Title is req').notEmpty();
   // req.checkBody('author','author is req').notEmpty();
-  req.checkBody('category','category is req').notEmpty();
-  req.checkBody('body','body is req').notEmpty();
+  req.checkBody('category', 'category is req').notEmpty();
+  req.checkBody('body', 'body is req').notEmpty();
 
   let errors = req.validationErrors();
-  if(errors){
-    Category.getCategories((err,categories)=>{
-      res.render('add_article',{
-        errors:errors,
-        title:'Create Article',
-        categories:categories
+  if (errors) {
+    Category.getCategories((err, categories) => {
+      res.render('add_article', {
+        errors: errors,
+        title: 'Create Article',
+        categories: categories,
+        // author:author
       });
     });
+  } else {
+    // const auth = await User.findById(req.body.user);
+    // console.log('hii');
+    // console.log(auth);
+    // console.log('hii');
+    let article = new Article2();
+    article.title = req.body.title;
+    article.subtitle = req.body.subtitle;
+    article.category = req.body.category;
+    article.body = req.body.body;
 
-  }
-  else{
-    let article =new Article2();
-    article.title=req.body.title;
-    article.subtitle=req.body.subtitle;
-    article.category=req.body.category;
-    article.body=req.body.body;
-    article.user=req.user.id;
-    // article.author=req.body.author;
+    article.user = req.user.id;
+
+    // article.author=auth;
     // category.description=req.body.description;
 
-    Article2.addArticle(article,(err, article)=>{
-      if(err){
+    Article2.addArticle(article, (err, article) => {
+      if (err) {
         res.send(err);
       }
-      req.flash('success','You Article will be added when its Verified');
+      req.flash('success', 'You Article will be added when its Verified');
       res.redirect('/manage/articles');
     });
   }
-
 });
 
 //edit articlse -post
-router.post('/edit/:id' ,ensureAuth, (req,res,next)=>{
-  req.checkBody('title','Title is req').notEmpty();
+router.post('/edit/:id', ensureAuth, (req, res, next) => {
+  req.checkBody('title', 'Title is req').notEmpty();
   // req.checkBody('author','author is req').notEmpty();
-  req.checkBody('category','category is req').notEmpty();
-  req.checkBody('body','body is req').notEmpty();
+  req.checkBody('category', 'category is req').notEmpty();
+  req.checkBody('body', 'body is req').notEmpty();
 
   let errors = req.validationErrors();
-  if(errors){
-    Category.getCategories((err,categories)=>{
-      res.render('edit_article',{
-        errors:errors,
-        title:'Edit Article',
-        categories:categories
+  if (errors) {
+    Category.getCategories((err, categories) => {
+      res.render('edit_article', {
+        errors: errors,
+        title: 'Edit Article',
+        categories: categories,
       });
     });
-  }
-  else{
-    let article =new Article();
-    const query = {_id: req.params.id}
+  } else {
+    let article = new Article();
+    const query = { _id: req.params.id };
     const update = {
-      title:req.body.title,
-      subtitle:req.body.subtitle,
-      category:req.body.category,
+      title: req.body.title,
+      subtitle: req.body.subtitle,
+      category: req.body.category,
       // author:req.body.author,
-      body:req.body.body
-    }
+      body: req.body.body,
+    };
 
     // category.description=re q.body.description;
 
-    Article.updateArticle(query,update,{},(err, article)=>{
-      if(err){
+    Article.updateArticle(query, update, {}, (err, article) => {
+      if (err) {
         res.send(err);
       }
-      req.flash('success','Article Updated');
+      req.flash('success', 'Article Updated');
       res.redirect('/manage/articles');
     });
   }
-
-
 });
-
 
 //delete article
 
-router.delete('/delete/:id',ensureAuth,(req,res,next)=>{
+router.delete('/delete/:id', ensureAuth, (req, res, next) => {
   // let category =new Category();
-  const query ={_id: req.params.id}
+  const query = { _id: req.params.id };
   // const update ={title :req.body.title, description:req.body.description}
 
-  Article.removeArticle(query,(err, article)=>{
-    if(err){
+  Article.removeArticle(query, (err, article) => {
+    if (err) {
       res.send(err);
     }
     // res.redirect('/manage/categories');
     res.status(200);
   });
-
 });
 
-router.post('/comments/add/:id',(req,res,next)=>{
-  req.checkBody('comment_subject','subject is req').notEmpty();
-  req.checkBody('comment_author','author is req').notEmpty();
+router.post('/comments/add/:id', (req, res, next) => {
+  req.checkBody('comment_subject', 'subject is req').notEmpty();
+  req.checkBody('comment_author', 'author is req').notEmpty();
   // req.checkBody('category','category is req').notEmpty();
-  req.checkBody('comment_body','Body is req').notEmpty();
+  req.checkBody('comment_body', 'Body is req').notEmpty();
 
-  let errors=req.validationErrors();
+  let errors = req.validationErrors();
 
-  if(errors){
-    Article.getArticleById(req.params.id, (err, article)=>{
+  if (errors) {
+    Article.getArticleById(req.params.id, (err, article) => {
       // console.log(article.title);
-      res.render('article',{
+      res.render('article', {
         title: 'Article',
         article: article,
-        errors:errors
+        errors: errors,
       });
     });
-  }
+  } else {
+    let article = new Article();
+    let query = { _id: req.params.id };
 
-  else{
-    let article=new Article();
-    let query={_id:req.params.id}
-
-    let comment={
+    let comment = {
       comment_subject: req.body.comment_subject,
       comment_author: req.body.comment_author,
       comment_body: req.body.comment_body,
       comment_email: req.body.comment_email,
-    }
+    };
 
-    Article.addComment(query,comment,(err,article)=>{
-      res.redirect('/articles/show/'+req.params.id);
+    Article.addComment(query, comment, (err, article) => {
+      res.redirect('/articles/show/' + req.params.id);
     });
   }
 });
